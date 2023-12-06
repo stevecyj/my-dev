eae31cddb23491cbf4d17c9c6aa4bee9bd9f178c
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-12-06 17:26:55 +0800

    chore: update URLs and project information in Docusaurus config
    - Update the `url` field in `docusaurus.config.ts` to `https://stevecyj.github.io`
    - Update the `baseUrl` field in `docusaurus.config.ts` to `/my-dev/`
    - Update the `organizationName` field in `docusaurus.config.ts` to `stevecyj`
    - Update the `projectName` field in `docusaurus.config.ts` to `my-dev`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://stevecyj.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-dev/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'stevecyj', // Usually your GitHub org/user name.
+  projectName: 'my-dev', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
